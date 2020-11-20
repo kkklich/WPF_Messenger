1abcc17687aa6940270c50be372dcbb8c3ac9edd
kkklich <32643161+kkklich@users.noreply.github.com>
2020-11-20 20:33:18 +0100

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_Messenger
+{
+    class Class1
+    {
+        public int Number { get; set; }
+        public string User { get; set; }
+        public string Text{ get; set; }
+        public string Time { get; set; }
+
+    }
+
+
+   
+}
