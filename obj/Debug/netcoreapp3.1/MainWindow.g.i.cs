f46e5744ba82fba20c7c8cb6121a9d96d8bee5c4
kkklich <32643161+kkklich@users.noreply.github.com>
2020-11-20 20:35:25 +0100

    wpfMessenger
    Count fb's messages from html files

--- obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -165,7 +165,7 @@ namespace WPF_Messenger {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WPF_Messenger;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WPF_Messenger;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
